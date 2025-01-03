c84db2129b0658680ac8ea37921dbfd4f2a03024
Stella Kontopoulou <kontopouloustella@yahoo.ie>
2025-01-03 16:19:26 +0200

    initilal nav-bar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'nav-bar',
+        component: NavBarComponent
+    }
+];
