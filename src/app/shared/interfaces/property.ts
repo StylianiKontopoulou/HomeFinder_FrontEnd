8cc5c3208d084f17f38345df248fe564312d9861
Stella Kontopoulou <kontopouloustella@yahoo.ie>
2025-01-24 22:02:36 +0200

    add interfaces

---
+++ src/app/shared/interfaces/property.ts
@@ -0,0 +1,10 @@
+export interface Property {
+    title: string;
+    description: string;
+    yearOfConstruction: number;
+    price: number;
+    squareMeters: number;
+    floor: number;
+    bathrooms: number;
+    bedrooms: number;
+}
---
+++ src/app/shared/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    userName: string;
+    email: string;
+    password: string;
+}
+
+export interface LoggedInUser {
+    fullname: string;
+    email: string;
+}
